97e80d5dfdae576412c969d3396a1ca56ecdc5fe
dell <574817827@qq.com>
2020-06-17 18:53:44 +0800

    modify client for key gen

--- client/main.go
+++ client/main.go
@@ -20,43 +20,43 @@ func NewApp() *cli.App {
 				Action: func(c *cli.Context) error {
 					host := c.Args().First()
 					if err := gredis.Setup(host, "123456"); err != nil {
-						fmt.Println("redis error with ", host)
+						//fmt.Println("redis error with ", host)
 						return err
 					}
 
 					count, err := strconv.Atoi(c.Args().Get(1))
 					if err != nil {
-						fmt.Println(count, " error count")
+						//fmt.Println(count, " error count")
 						return err
 					}
 					if count%2 != 0 {
-						fmt.Println("count must be even")
+						//fmt.Println("count must be even")
 						return nil
 					}
 					masterKey := c.Args().Get(2)
 					addrs, err := GenerateAddress(masterKey, count, "/0/0", &chaincfg.MainNetParams, true)
 					if err != nil {
-						fmt.Println("generate address error with ", masterKey)
+						//fmt.Println("generate address error with ", masterKey)
 						return err
 					}
 					//slot_index_long
 					//slot_index_short
 					index := 0
 					for index < count/2 {
-						index += 2
-						longAddr := addrs[index]
-						shortAddr := addrs[index+1]
+						longAddr := addrs[2*index]
+						shortAddr := addrs[2*index+1]
 						longKey := "slot_" + strconv.Itoa(index) + "_long"
 						shortKey := "slot_" + strconv.Itoa(index) + "_short"
+						index++
 						fmt.Printf("set %s %s \n", longKey, longAddr)
 						fmt.Printf("set %s %s \n", shortKey, shortAddr)
 						err = gredis.Set(longKey, longAddr, 0)
 						if err != nil {
-							fmt.Printf("longKey写入错误, %s \n", err)
+							//fmt.Printf("longKey写入错误, %s \n", err)
 						}
 						err = gredis.Set(shortKey, shortAddr, 0)
 						if err != nil {
-							fmt.Printf("shortKey写入错误, %s \n", err)
+							//fmt.Printf("shortKey写入错误, %s \n", err)
 						}
 					}
 					return nil
